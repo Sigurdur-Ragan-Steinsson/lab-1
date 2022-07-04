0969a376800ee45945eca327b58dfbdbc0a80738
Sar30 @ <108635097+Sigurdur-Ragan-Steinsson@users.noreply.github.com>
2022-07-04 21:38:26 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream> // include iostream
+#include <iomanip> // include iomanip
+#include <string> // include string
+
+using namespace std; 
+
+int main()
+{
+int result = 10800; // 3*60*60
+int three_mins_to_secs = 3 * 60;// 3 minutes in seconds
+int six_mins_in_secs = 6 * 60;// 6 minutes in seconds
+float pi = 3.14152926; // it is value of pi
+
+cout << "result: " << result << endl; // print result (whatever the result is)
+}
